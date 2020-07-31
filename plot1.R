d4e60f7dcd1caa26cf24ffb2424abd847e9000f4
sergiomr04 <sermalrod@gmail.com>
2020-07-30 23:16:26 -0500

    Update y label and scale

--- plot1.R
+++ plot1.R
@@ -25,9 +25,10 @@ Total_emision<-NEI%>%group_by(year)%>%summarize(Emissions=sum(Emissions))%>%prin
 #plotting
 png(filename='plot1.png')
 color_range <- colorRampPalette(c("blue","green"))
+par(mar = c(4, 5.5, 2, 1), oma = c(0, 0, 2,0))
 x<-barplot(height = Total/10^6
         , names.arg = names(Total)
-        , xlab = "Years", ylab = expression("Emissions (10"^6*") Tons")
+        , xlab = "Years", ylab = expression("Emissions (10"^6*" Tons)")
         , col = color_range(4), ylim=c(0,8.5)
         , main = expression('Annual Emission PM'[2.5]*' from 1999 to 2008'))
 text(x =x , y = round(Total/10^6,4)
